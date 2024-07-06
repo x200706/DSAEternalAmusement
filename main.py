4c15ff6cd7b879e6a6e510746c59401238c10bbf
Crystal <99391710+x200706@users.noreply.github.com>
2024-07-06 08:56:59 +0000

    更新註解

---
+++ main.py
@@ -0,0 +1,14 @@
+# # Python DSA Cheat Sheet
+# 用Python練習演算法題目的代價是
+# 雖然簡短，但要去了解不熟悉的API
+# 這個地方提供整理常用的演算法解題內建工具：
+
+
+# 參考資料
+# https://www.stationx.net/python-data-structures-cheat-sheet/
+
+# 創stack queue set heap
+# 檢查element有沒有在資料結構內
+# pop push
+# substring的方法
+# 建立實體並傳入（TreeNode用）
