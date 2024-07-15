2ea7e7683cce7973daed9bbc2971fa95b99e93c8
Crystal <99391710+x200706@users.noreply.github.com>
2024-07-15 15:33:12 +0000

    加遞迴展開註釋

--- factorial.py
+++ factorial.py
@@ -6,3 +6,9 @@ def factorial(n):
     return n * factorial(n-1)
     
 print(factorial(6))
+# 6 * f(5)
+# 6 * 5 * f(4)
+# 6 * 5 * 4 * f(3)
+# 6 * 5 * 4 * 3 * f(2)
+# 6 * 5 * 4 * 3 * 2 * f(1)
+# 6 * 5 * 4 * 3 * 2 * 1 = 720
