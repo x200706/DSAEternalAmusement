7661ab138a92e090775d701788787709a3dba1b6
Crystal <99391710+x200706@users.noreply.github.com>
2024-07-21 07:48:09 +0000

    最大子陣列（暴力解）

---
+++ maxSubArray.py
@@ -0,0 +1,26 @@
+list = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# 幾個特殊情境：
+# 1. list[8:8] == []
+# 2. list[j:i]，i得>j不然也是得到空陣列
+# 基於以上兩點等下巢狀迴圈可設計i > j，可以少跑出幾個空陣列減少運行時間
+# （雖然迴圈照跑就是了...）
+
+# 暴力解
+def maxSubArr(list):
+  temp_list = []
+  temp_sum = ''
+  # create every SubArray
+  for i in reversed(range(0, len(list)+1)):  # j == 8 7 6 5 4 3 2 1 0
+    for j in range(0, len(list)):  # j == 0 1 2 3 4 5 6 7 8
+      if i > j:
+        new_list = list[j:i] # 小心處理左開右閉!!
+        print('when i =' + str(i) + ' j =' + str(j))
+        print(new_list)
+        new_list_sum = sum(new_list)
+        # 首次加總或新總和比就有陣列還大，取代之
+        if temp_sum == '' or new_list_sum > temp_sum: 
+          temp_sum = new_list_sum
+          temp_list = new_list
+
+  return {temp_sum : temp_list}
+print(maxSubArr(list))
